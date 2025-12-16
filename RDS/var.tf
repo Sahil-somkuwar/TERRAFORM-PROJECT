4e91fdd3f60620710790dfd36563f46f0faad000
SAHIL SOMKUWAR <sahilsomkuwar10@gmail.com>
2025-12-16 14:09:29 +0530

    files

---
+++ RDS/var.tf
@@ -0,0 +1,8 @@
+variable "username" {
+    default = "Aditya"     ##---------> this value goes into rds.tf file line (12)
+}
+
+variable "password" {
+    default = "Aditya123"  ##---------> this value goes into rds.tf file line (13)
+  
+}
